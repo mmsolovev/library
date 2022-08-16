2d2004e3c482ae8cf94938beeeb0ff038b800197
mmsolovev <mmsolovevmail@gmail.com>
2022-08-16 19:14:14 +0300

    add: work with CRUD

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from products.models import Book
@@ -12,7 +12,7 @@ class BookView(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author']
     ordering_fields = ['price', 'author']
