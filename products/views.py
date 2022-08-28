0e1204b40efc1c3912dbbbe15640aee61f8b32e8
Mikhail Solovev <81823776+mmsolovev@users.noreply.github.com>
2022-08-28 18:26:47 +0300

    add: work with permissions (#8)
    Co-authored-by: mmsolovev <mmsolovevmail@gmail.com>

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from products.models import Book
+from products.permissions import IsOwnerOrStaffOrReadOnly
 from products.serializers import BookSerializer
 
 
@@ -12,10 +12,14 @@ class BookView(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author']
     ordering_fields = ['price', 'author']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 
 def auth(request):
