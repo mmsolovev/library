9b576a0051032789eb8edd2710f089e119946ac7
mmsolovev <mmsolovevmail@gmail.com>
2022-08-31 21:04:34 +0300

    add: annotate

--- products/tests/test_serializers.py
+++ products/tests/test_serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Case, When
 from django.test import TestCase
 
 from products.models import Book
@@ -8,19 +9,26 @@ class BookSerializerTestCase(TestCase):
     def test_ok(self):
         book_1 = Book.objects.create(price=300, name='Bookname 1', author='Author 1')
         book_2 = Book.objects.create(price=200, name='Bookname 2', author='Author 2')
-        data = BookSerializer([book_1, book_2], many=True).data
+        books = Book.objects.all().annotate(
+            annotted_likes=Count(Case(When(userbookrelation__like=True, then=1))).order_by('id')
+        )
+        data = BookSerializer(books, many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'price': 300,
                 'name': 'Bookname 1',
                 'author': 'Author 1',
+                'likes_count': 0,
+                'annotated_likes': 3,
             },
             {
                 'id': book_2.id,
                 'price': 200,
                 'name': 'Bookname 2',
                 'author': 'Author 2',
+                'likes_count': 0,
+                'annotated_likes': 3,
             },
         ]
         self.assertEqual(expected_data, data)
